b16886b17da810d10c13587535e785b2d4921c3a
Adam Chase <adam.chase@cgius.com>
2018-04-30 15:10:47 -0500

    Test variable to see if gitignore works

--- src/main/java/com/achase/BattleBot/Ref.java
+++ src/main/java/com/achase/BattleBot/Ref.java
@@ -3,4 +3,5 @@ package com.achase.BattleBot;
 public class Ref {
 	public static final String token = Token.token;
 	public static final String prefix = "!";
+	public static final String testVar = "test";
 }
