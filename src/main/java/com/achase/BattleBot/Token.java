a07419ba1161a44d166d31da3f29996e382de902
Adam Chase <adam.chase@cgius.com>
2018-04-30 15:03:35 -0500

    Added newly generated token

--- src/main/java/com/achase/BattleBot/Token.java
+++ src/main/java/com/achase/BattleBot/Token.java
@@ -1,5 +1,5 @@
 package com.achase.BattleBot;
 
 public class Token {
-	public static final String token = "NDM5MTI2OTA5ODUxNDAyMjQz.DcOoug.fxbL8a0a_jADqRaPXg99ESAzVX0";
+	public static final String token = "NDM5MTI2OTA5ODUxNDAyMjQz.DcixpA.p3wpv_AxemC-pHVXbkdLqpUzUZA";
 }
